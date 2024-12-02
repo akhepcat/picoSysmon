bfee8fc4d5be0aea4148d7801dbbfdbaf8c39973
Leif Sawyer <leif@akhepcat.com>
2024-12-02 11:42:18 -0900

    small cleanup here

--- secrets/__init__.py
+++ secrets/__init__.py
@@ -1,6 +1,6 @@
-""" this is just the empty initfile, all the secrets are in CONFIG.py """
+""" this init contains validation for the secrets are in CONFIG.py """
 
-# from .CONFIG import CONFIG
+from .CONFIG import CONFIG
 
 # We don't import any functions, just some variable definitions
 __all__ = [

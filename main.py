2b2f256757fcd69ad0a6d077469400ae0a64331f
Leif Sawyer <leif@akhepcat.com>
2024-12-02 11:40:37 -0900

    Add validation checks to the startup

--- main.py
+++ main.py
@@ -13,6 +13,26 @@ except:
     exit(1)
 
 if __name__  == "__main__":
+    if (CONFIG.SSID == "") or (CONFIG.SSID is None):
+        print("ERROR: SSID not defined in secrets.CONFIG")
+        exit(1)
+
+    if (CONFIG.PSK == "") or (CONFIG.PSK is None):
+        print("ERROR: WIFI PSK not defined in secrets.CONFIG")
+        exit(1)
+
+    if (CONFIG.COUNTRY == "") or (CONFIG.COUNTRY is None):
+        print("ERROR: WIFI COUNTRY not defined in secrets.CONFIG")
+        exit(1)
+
+    if (CONFIG.INFLUXURL == "") or (CONFIG.INFLUXURL is None):
+        print("ERROR: INFLUXURL not defined in secrets.CONFIG")
+        exit(1)
+
+    if (CONFIG.HOSTNAME == "") or (CONFIG.HOSTNAME is None):
+        print("WARN: HOSTNAME not defined in secrets.CONFIG, using default 'pico2w'")
+        CONFIG.HOSTNAME = "pico2w"
+
     sysmon = picoSysmon(CONFIG.DEBUG, CONFIG.SSID, CONFIG.PSK, CONFIG.COUNTRY, CONFIG.INFLUXURL, CONFIG.TOKEN, CONFIG.HOSTNAME)
     sysmon.run()
     exit(0)

af4f360d65dfdc92bd4bf6a846077ca79451c2b4
Leif Sawyer <leif@akhepcat.com>
2024-12-04 10:55:35 -0900

    don't need the tail for the import line

--- secrets/__init__.py
+++ secrets/__init__.py
@@ -1,6 +1,6 @@
 """ this init contains validation for the secrets are in CONFIG.py """
 
-from .CONFIG import CONFIG
+from . import CONFIG
 
 # We don't import any functions, just some variable definitions
 __all__ = [

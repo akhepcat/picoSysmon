6f929f6ac3c840d588fadcf09a7bece8878b4c2f
Leif Sawyer <leif@akhepcat.com>
2024-12-13 09:37:04 -0900

    define the BME680 config variables

--- secrets/CONFIG.py
+++ secrets/CONFIG.py
@@ -7,5 +7,8 @@ INFLUXURL = ""	# the full url for posting to your InfluxDB;  if you don't use a 
 TOKEN = ""	# empty, string or None, but for influx v2, this is your API token; code will add it to the Headers if supplied
 HOSTNAME = "mypico2w-{MAC}"		# regular hostname, or template with the mac address using the format "host-{MAC}"  where {MAC} will be automatically replaced inline
 
+BME680_SDA = 0      # gpio pin number for BME680 data;  0 disables
+BME680_SCL = 0      # gpio pin number for BME680 clock; 0 disables
+
 # This prints a lot of debugging info to the console if true
 DEBUG = True

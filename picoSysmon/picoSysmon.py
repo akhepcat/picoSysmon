f6a74d836b82bf1091e400daf7975e576bb23085
Leif Sawyer <leif@akhepcat.com>
2024-12-18 14:54:13 -0900

    clean up debug logging a bit so its simplified into a single function

--- picoSysmon/picoSysmon.py
+++ picoSysmon/picoSysmon.py
@@ -48,7 +48,7 @@ class picoSysmon:
         self.startup = self.__now()
         self.tempcache = 0
         self.prescache = 0
-        self.debug = 0
+        self.debug = debug
         if (int(bmesda)):
             self.bmesda = int(bmesda)
         else:
@@ -58,16 +58,16 @@ class picoSysmon:
         else:
             self.bmescl = 0
 
-        # Always debug when the USB serial console is detected
-        if self.__usbDetect():
-            print("Console debugging detected and enabled")
-            self.debug = 1
+        # Always debug when the USB serial console is detected (this is not quite working)
+#        if self.__usbDetect():
+#            self.__logprt("Console debugging detected and enabled")
+#            self.debug = 1
 
         if re.search(r"\{MAC\}", hostname):
             MAC = self.wlan.config('mac').hex(":")
             MAC = MAC.replace(":","")
             MAC = MAC[6:]
-            if self.debug: print(f"updating hostname with mac: {MAC}")
+            self.__logprt(f"updating hostname with mac: {MAC}")
             self.HOSTNAME = self.HOSTNAME.replace("{MAC}", MAC)
 
         if (token is None) or (token == ""):
@@ -84,7 +84,7 @@ class picoSysmon:
         # configure the network data appropriately the first time
         network.country(country)
         network.hostname(self.HOSTNAME)
-        if self.debug: print(f"My hostname is: {self.HOSTNAME}")
+        self.__logprt(f"My hostname is: {self.HOSTNAME}")
 
         # Last, set non-user self vars
         self.temp_sensor = ADC(ADC.CORE_TEMP)    # more portable across microcontrollers
@@ -99,8 +99,9 @@ class picoSysmon:
     def __now(self):
         return( mktime(gmtime()) )
 
-    def __lprint(self, line: str):
-        print(str(self.__now()) + ': ' + line)
+    def __logprt(self, line: str):
+        if self.debug:
+            print(str(self.__now()) + ': ' + line)
         lfile = open("logfile.txt", "a")
         lfile.write(str(self.__now()) + ': ' + line + '\n')
         lfile.flush()
@@ -113,7 +114,7 @@ class picoSysmon:
         SIE_CONNECTED  = 1 << 16
         SIE_SUSPENDED  = 1 << 4
         usbConnected   = (mem32[SIE_STATUS_REG] & (SIE_CONNECTED | SIE_SUSPENDED))
-        # self.__lprint(f"usbConnected = {usbConnected}")
+        self.__logprt(f"usbConnected = {usbConnected}")
         if ((usbConnected | SIE_CONNECTED) == SIE_CONNECTED ) or ((usbConnected | SIE_SUSPENDED) == SIE_SUSPENDED):
             return(True)
         else:
@@ -135,7 +136,7 @@ class picoSysmon:
             while sleeps < 5:
                 if self.wlan.isconnected() == True:
                     self.ip = self.wlan.ifconfig()[0]
-                    if self.debug: print(f'Connected on {self.ip}')
+                    self.__logprt(f'Connected on {self.ip}')
                     return self.wlan
                 sleeps += 1
                 sleep(1)
@@ -148,53 +149,50 @@ class picoSysmon:
 
 
     def __post_data(self, data):
-        if self.debug: print("posting data to influxdb...")
-        if self.debug: print(data)
+        self.__logprt(f"posting data to influxdb...\n{data}")
         try:
             response = requests.post(self.INFLUXURL, headers=self.headers, data=data, timeout=5)
         except:
             self.webtimeouts += 1
-            if self.debug: print(f"timeout #{self.webtimeouts} sending data to influxdb")
+            self.__logprt(f"timeout #{self.webtimeouts} sending data to influxdb")
             return(False)
         if response.status_code == 204:
-            if self.debug: print("Data posted successfully")
+            self.__logprt("Data posted successfully")
             # assume that a success resets things
             self.webtimeouts = 0
             ret = True
         else:
-            if self.debug:
-                print("Failed to post data:")
-                print("Status Code:", response.status_code)
-                print("Response:", response.text)
+            self.__logprt(f"Failed to post data! http status Code is {response.status_code}")
+            self.__logprt("Response text was:\n{response.text}")
             ret = False
         response.close()
         return(ret)
 
 
     def __update_mem(self):
-        if self.debug: print("updating mem info")
+        self.__logprt("updating mem info")
         free = gc.mem_free()
         used = gc.mem_alloc()
         max = used + free
-        if self.debug: print(f"memory is using {used} bytes out of {max}")
+        self.__logprt(f"memory is using {used} bytes out of {max}")
         data = f"memory,host={self.HOSTNAME} totalmem={max}\n" + f"memory,host={self.HOSTNAME} usedmem={used}\n" + f"memory,host={self.HOSTNAME} freemem={free}"
         return(data)
 
 
     def __update_disk(self):
-        if self.debug: print("updating disk info")
+        self.__logprt("updating disk info")
         s = os.statvfs('//')
-        if self.debug: print(f"statvfs returns {s}")
+        self.__logprt(f"statvfs returns {s}")
         max = ( s[0]*s[2] )
         free = ( s[0]*s[3] )
         used = max - free
-        if self.debug: print(f"disk is using {used} bytes out of {max}, with {free} remaining")
+        self.__logprt(f"disk is using {used} bytes out of {max}, with {free} remaining")
         data = f"disk_usage,host={self.HOSTNAME},drive=flash,mount=/ size={max}\n" + f"disk_usage,host={self.HOSTNAME},drive=flash,mount=/ free={free}"
         return(data)
 
 
     def __update_temp(self):
-        if self.debug: print("updating temp info")
+        self.__logprt("updating temp info")
         # Read the raw ADC value
         adc_value = self.temp_sensor.read_u16()
         # Convert ADC value to voltage
@@ -202,22 +200,22 @@ class picoSysmon:
         # Temperature calculation based on sensor characteristics
         temp = 27 - (voltage - 0.706) / 0.001721
         temp = temp * 1000  # we scale in grafana
-        if self.debug: print(f"Read temp: {temp}C")
+        self.__logprt(f"Read temp: {temp}C")
         data = f"thermals,host={self.HOSTNAME},zone=cpu temp={temp}"
         return(data)
 
 
     def __update_uptime(self):
-        if self.debug: print("updating uptime info")
+        self.__logprt("updating uptime info")
         self.uptime = self.__now() - self.startup
-        if self.debug: print(f"uptime: {self.uptime} seconds")
+        self.__logprt(f"uptime: {self.uptime} seconds")
         data = f"system,host={self.HOSTNAME} uptime={self.uptime}"
         return(data)
 
 
     def __update_sensors(self):
         if (self.bmesda > 0):
-            if self.debug: print("updating sensor info")
+            self.__logprt("updating sensor info")
             try:
                 bme = bme680.BME680_I2C( I2C(id=0, scl=Pin(self.bmescl), sda=Pin(self.bmesda) ), debug=False )
             except:
@@ -228,7 +226,7 @@ class picoSysmon:
 
 #            temp = bme.temperature
 #            if temp is None:
-#                if self.debug: print("BME680: oh, we got none temp value")
+#                self.__logprt("BME680: oh, we got none temp value")
 #                return("")
             for _ in range(3):              # take 3 measurements for stability, and use the last one
                 temp=bme.temperature
@@ -244,8 +242,8 @@ class picoSysmon:
             voc = round(voc, 2)                   # inverse VOC concentration of ethanol, CO, etc.: high resistance=low concentration
 
             if press < 630:    # this would be lower than "human comfortable" but if you're putting this on a high-altitude baloon, it might be too high
-                if self.debug: print(f"press returned invalid: {press}")
-            if self.debug: print(f"temp: {temp}  humidity: {humid}  press: {press}  voc: {voc}")
+                self.__logprt(f"press returned invalid: {press}")
+            self.__logprt(f"temp: {temp}  humidity: {humid}  press: {press}  voc: {voc}")
             data = f"environmental,host={self.HOSTNAME} temp={temp}\n" + f"environmental,host={self.HOSTNAME} humid={humid}\n" + f"environmental,host={self.HOSTNAME} voc={voc}\n" + f"environmental,host={self.HOSTNAME} press={press}"
             return(data)
         else:
@@ -257,7 +255,7 @@ class picoSysmon:
             while True:
                 # Wifi Setup (each time!)
                 self.blink.init(freq=25, mode=Timer.PERIODIC, callback=self.__blinken)    # freq is events per second
-                if self.debug: print("Connecting to wifi")
+                self.__logprt("Connecting to wifi")
                 self.__connect()
                 sleep(2)     # let things stabilize
                 self.blink.deinit()
@@ -281,14 +279,14 @@ class picoSysmon:
                 self.blink.deinit()
 
                 # Kill the wifi, then sleep between loops
-                if self.debug: print("Deactivating wifi")
+                self.__logprt("Deactivating wifi")
                 self.wlan.disconnect()
                 self.wlan.active(False)
                 self.blink.init(freq=1, mode=Timer.PERIODIC, callback=self.__blinken)
                 sleeps = ( 60 * 5 )
                 if self.debug:
                     sleeps = 60
-                if self.debug: print(f"sleeping for {sleeps} seconds")
+                self.__logprt(f"sleeping for {sleeps} seconds")
                 gc.collect()
                 sleep(sleeps)
                 self.blink.deinit()
@@ -296,7 +294,7 @@ class picoSysmon:
         except KeyboardInterrupt:
             if (self.__usbDetect()):
                 # This should quit back to Thonny
-                print("I'm halting for you...")
+                self.__logprt("I'm halting for you...")
                 exit(0)
             else:
                 # This shouldn't occur, but just returns back to main()
